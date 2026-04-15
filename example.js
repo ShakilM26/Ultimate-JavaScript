b98bfd4d360192bbaa11b6f0e9cdfcd615491a7a
ShakilM26 <shakil1emperor@gmail.com>
2026-04-15 23:21:47 +0600

    Variable

---
+++ example.js
@@ -0,0 +1,17 @@
+console.log('Hello');
+
+document.getElementById('title').innerHTML = 'Hello World';
+
+let sum = 50+50;
+console.log(sum);
+
+if (sum >= 100) {
+    console.log('Expensive');
+} else {
+    console.log('Cheap')
+}
+
+function sayHello(name) {
+    console.log(`Hello ${name}`);
+}
+sayHello('Sejuti');
